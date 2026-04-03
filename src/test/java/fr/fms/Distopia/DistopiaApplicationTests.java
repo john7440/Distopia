bf04166663c4e20cf5edd268e248c8c0fbb3cea7
Jonathan M. <neodev7440@gmail.com>
2026-04-03 13:33:14 +0200

    first commit

---
+++ src/test/java/fr/fms/Distopia/DistopiaApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.fms.Distopia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistopiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
