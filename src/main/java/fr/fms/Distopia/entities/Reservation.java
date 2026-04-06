5c34afd161695fa835337cffc12683ae6ebed4f2
Jonathan M. <neodev7440@gmail.com>
2026-04-06 07:55:32 +0200

    ajout des enttées Reservation et seance

---
+++ src/main/java/fr/fms/Distopia/entities/Reservation.java
@@ -0,0 +1,28 @@
+package fr.fms.Distopia.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor @NoArgsConstructor
+public class Reservation implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private LocalDateTime reservedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "seance_id", nullable = false)
+    private Seance seance;
+}
---
+++ src/main/java/fr/fms/Distopia/entities/Seance.java
@@ -0,0 +1,27 @@
+package fr.fms.Distopia.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor @NoArgsConstructor
+public class Seance implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private LocalDateTime dateTime;
+
+    private int availableSeats;
+    private double price;
+
+    @ManyToOne
+    @JoinColumn(name = "movie_id", nullable = false)
+    private Movie movie;
+}
