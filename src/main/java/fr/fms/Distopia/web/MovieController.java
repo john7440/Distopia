3dfd126faf19ca20cf5b66e4d6d5bb38d2393653
Jonathan M. <neodev7440@gmail.com>
2026-04-06 12:33:17 +0200

    ajout du mapping pour la gestion des films

--- src/main/java/fr/fms/Distopia/web/MovieController.java
+++ src/main/java/fr/fms/Distopia/web/MovieController.java
@@ -2,6 +2,8 @@ package fr.fms.Distopia.web;
 
 import fr.fms.Distopia.service.CinemaService;
 import fr.fms.Distopia.service.MovieService;
+import fr.fms.Distopia.utils.SessionUtils;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,5 +23,14 @@ public class MovieController {
         model.addAttribute("movies", movieService.getByCinema(cinemaId));
         model.addAttribute("cinemaId", cinemaId);
         return "movies";
+    }
+
+    //--------------page de gestion des films---------------------
+    @GetMapping("/admin/movies")
+    public String adminMovies(Model model, HttpSession session){
+        if(SessionUtils.isNotAdmin(session)) return  SessionUtils.REDIRECTION;
+        model.addAttribute("movies", movieService.getAll());
+        model.addAttribute("cinemas", cinemaService.getAll());
+        return "admin-movies";
     }
 }
