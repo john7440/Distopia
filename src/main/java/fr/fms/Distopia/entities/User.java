9b358f9b1b2ca9f1cc785ab76659ae5872ec4608
Jonathan M. <neodev7440@gmail.com>
2026-04-06 07:32:57 +0200

    ajout de l'entité user

---
+++ src/main/java/fr/fms/Distopia/entities/User.java
@@ -0,0 +1,25 @@
+package fr.fms.Distopia.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name="users")
+@Data
+@AllArgsConstructor @NoArgsConstructor
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role = Role.USER;
+}
