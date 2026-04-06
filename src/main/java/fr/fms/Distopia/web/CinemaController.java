4f1b2ca81b561413bd402138cdfd6fddd32da148
Jonathan M. <neodev7440@gmail.com>
2026-04-06 11:34:18 +0200

    ajout des méthode pour la recherche par mot clé + gestion cinémas poour admin

--- src/main/java/fr/fms/Distopia/web/CinemaController.java
+++ src/main/java/fr/fms/Distopia/web/CinemaController.java
@@ -2,6 +2,8 @@ package fr.fms.Distopia.web;
 
 import fr.fms.Distopia.service.CinemaService;
 import fr.fms.Distopia.service.TownService;
+import fr.fms.Distopia.utils.SessionUtils;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Controller;
@@ -28,6 +30,24 @@ public class CinemaController {
             model.addAttribute("selectedTownId", townId);
         }
         return "cinemas";
+    }
+
+    //------recherche par mot-clé-------------------
+    @GetMapping("/cinema/search")
+    public String searchCinemas(@RequestParam String keyword, Model model){
+        model.addAttribute("cinemas", cinemaService.search(keyword));
+        model.addAttribute("keyword", keyword);
+        return "cinemas";
+    }
+
+    //------------pourl admin - page de gestion des cinémas-------------
+    @GetMapping("/admin/cinemas")
+    public String cinemas(Model model, HttpSession session){
+        if (SessionUtils.isNotAdmin(session)) return SessionUtils.REDIRECTION; {
+            model.addAttribute("cinemas", cinemaService.getAll());
+            model.addAttribute("towns", townService.getAll());
+            return "admin-cinemas";
+        }
     }
 
 
