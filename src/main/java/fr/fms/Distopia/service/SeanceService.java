61e4c7f9a41cb58a00c5b466ea72328c32520b4d
Jonathan M. <neodev7440@gmail.com>
2026-04-07 10:20:39 +0200

    ajout des méthodes pour voir toutes les séances + créer/modifier une séance

--- src/main/java/fr/fms/Distopia/service/SeanceService.java
+++ src/main/java/fr/fms/Distopia/service/SeanceService.java
@@ -2,10 +2,12 @@ package fr.fms.Distopia.service;
 
 import fr.fms.Distopia.dao.MovieRepository;
 import fr.fms.Distopia.dao.SeanceRepository;
+import fr.fms.Distopia.entities.Movie;
 import fr.fms.Distopia.entities.Seance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -15,7 +17,28 @@ public class SeanceService {
     @Autowired
     private MovieRepository movieRepository;
 
+    //---------------les séances d'un film---------------------
     public List<Seance> getByMovie(Long movieId){
         return seanceRepository.findByMovieIdOrderByDateTimeAsc(movieId);
+    }
+
+    //------------toutes les séances-------------
+    public List<Seance> getAll(){
+        return seanceRepository.findAll();
+    }
+
+    //-------------------créer ou modifier une séance-----------------------
+    public Seance save(Long id, LocalDateTime dateTime, int availableSeats, double price, Long movieId) {
+        Seance seance = (id!=null) ? seanceRepository.findById(id).orElse(new Seance()) : new Seance();
+
+        seance.setDateTime(dateTime);
+        seance.setAvailableSeats(availableSeats);
+        seance.setPrice(price);
+
+        if(movieId !=null){
+            Movie movie = movieRepository.findById(movieId).orElseThrow();
+            seance.setMovie(movie);
+        }
+        return seanceRepository.save(seance);
     }
 }
