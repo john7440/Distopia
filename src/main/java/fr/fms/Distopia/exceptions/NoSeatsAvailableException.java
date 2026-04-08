5be46fc492248d708394d4987eac321d159c5c33
Jonathan M. <neodev7440@gmail.com>
2026-04-08 10:48:57 +0200

    ajout d'une execption personnalisées pour le nombre de sieges dispos

---
+++ src/main/java/fr/fms/Distopia/exceptions/NoSeatsAvailableException.java
@@ -0,0 +1,7 @@
+package fr.fms.Distopia.exceptions;
+
+public class NoSeatsAvailableException extends RuntimeException {
+    public NoSeatsAvailableException(String message) {
+        super(message);
+    }
+}
