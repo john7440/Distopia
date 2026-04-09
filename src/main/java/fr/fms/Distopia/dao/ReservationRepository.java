963d7ac7540a2b867eebaf732202c0bf28ed2193
Jonathan M. <neodev7440@gmail.com>
2026-04-09 10:30:58 +0200

    ajout de l'annontation repository + javadoc

--- src/main/java/fr/fms/Distopia/dao/ReservationRepository.java
+++ src/main/java/fr/fms/Distopia/dao/ReservationRepository.java
@@ -2,9 +2,14 @@ package fr.fms.Distopia.dao;
 
 import fr.fms.Distopia.entities.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+/**
+ * The JPA Repository of reservation
+ */
+@Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
     List<Reservation> findByUserIdOrderByReservedAtDesc(Long userId);
     boolean existsByUserIdAndSeanceId(Long userId, Long seanceId);
--- src/main/java/fr/fms/Distopia/dao/SeanceRepository.java
+++ src/main/java/fr/fms/Distopia/dao/SeanceRepository.java
@@ -7,11 +7,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-
+/**
+ * The JPA Repository of seances
+ */
+@Repository
 public interface SeanceRepository extends JpaRepository<Seance, Long> {
     List<Seance> findByMovieIdOrderByDateTimeAsc(Long movieId);
     //-------ajout d'un verrou pour éviter la race condition-------------
