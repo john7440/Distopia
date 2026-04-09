9f5269835b938fdb5e29098e0b081c57d0ed2bd7
Jonathan M. <neodev7440@gmail.com>
2026-04-09 07:16:34 +0200

    ajout de la javadoc

--- src/main/java/fr/fms/Distopia/exceptions/NoSeatsAvailableException.java
+++ src/main/java/fr/fms/Distopia/exceptions/NoSeatsAvailableException.java
@@ -1,5 +1,8 @@
 package fr.fms.Distopia.exceptions;
 
+/**
+ * Custom exception for available seats
+ */
 public class NoSeatsAvailableException extends RuntimeException {
     public NoSeatsAvailableException(String message) {
         super(message);
