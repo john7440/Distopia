43debae1da53390cfef83661b0430c4aa00fa9c1
Jonathan M. <neodev7440@gmail.com>
2026-04-09 09:00:22 +0200

    ajout de la javadoc

--- src/main/java/fr/fms/Distopia/entities/Town.java
+++ src/main/java/fr/fms/Distopia/entities/Town.java
@@ -9,6 +9,16 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Represents a Town (or City) entity in the application
+ * <p>
+ * This class serves as a geographical grouping for cinemas. It allows the
+ * application to filter and display cinemas based on their location
+ * <p>
+ * <strong>Note on Lombok:</strong> This class uses Lombok annotations
+ * ({@code @Data}, {@code @AllArgsConstructor}, {@code @NoArgsConstructor})
+ * to automatically generate getters, setters, and constructors
+ */
 @Entity
 @Data
 @AllArgsConstructor @NoArgsConstructor
