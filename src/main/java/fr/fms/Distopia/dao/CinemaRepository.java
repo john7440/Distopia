70052cb4997921e9b0f99d0d2bd6baace360ef94
Jonathan M. <neodev7440@gmail.com>
2026-04-09 10:29:38 +0200

    ajout de l'annontation repository + javadoc

--- src/main/java/fr/fms/Distopia/dao/CinemaRepository.java
+++ src/main/java/fr/fms/Distopia/dao/CinemaRepository.java
@@ -2,9 +2,14 @@ package fr.fms.Distopia.dao;
 
 import fr.fms.Distopia.entities.Cinema;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+/**
+ * The JPA Repository of cinemas
+ */
+@Repository
 public interface CinemaRepository extends JpaRepository<Cinema,Long> {
     List<Cinema> findByTownId(Long townId);
     List<Cinema> findByNameContainingIgnoreCaseOrAddressContainingIgnoreCase(String name,String address);
--- src/main/java/fr/fms/Distopia/dao/MovieRepository.java
+++ src/main/java/fr/fms/Distopia/dao/MovieRepository.java
@@ -2,9 +2,14 @@ package fr.fms.Distopia.dao;
 
 import fr.fms.Distopia.entities.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+/**
+ * The JPA Repository of movies
+ */
+@Repository
 public interface MovieRepository extends JpaRepository<Movie,Long> {
     List<Movie> findByCinemasIdAndDeletedFalse(Long cinemaId);
 }
