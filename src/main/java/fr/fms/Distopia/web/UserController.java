26f7c03a77ebd08e176502c3fb23284581582084
Jonathan M. <neodev7440@gmail.com>
2026-04-09 08:48:06 +0200

    retrait d'un import non utilisé

--- src/main/java/fr/fms/Distopia/web/UserController.java
+++ src/main/java/fr/fms/Distopia/web/UserController.java
@@ -15,8 +15,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.Optional;
-
 @Controller
 public class UserController {
 
