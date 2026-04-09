4c4dc234e7b2d2417b0a0d2de54e44a1ddc9b2a3
Jonathan M. <neodev7440@gmail.com>
2026-04-09 10:31:44 +0200

    ajout de l'annontation repository + javadoc

--- src/main/java/fr/fms/Distopia/dao/TownRepository.java
+++ src/main/java/fr/fms/Distopia/dao/TownRepository.java
@@ -3,6 +3,11 @@ package fr.fms.Distopia.dao;
 
 import fr.fms.Distopia.entities.Town;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+/**
+ * The JPA Repository of towns
+ */
+@Repository
 public interface TownRepository extends JpaRepository<Town,Long> {
 }
--- src/main/java/fr/fms/Distopia/dao/UserRepository.java
+++ src/main/java/fr/fms/Distopia/dao/UserRepository.java
@@ -2,9 +2,14 @@ package fr.fms.Distopia.dao;
 
 import fr.fms.Distopia.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+/**
+ * The JPA Repository of users
+ */
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByUsername(String username);
 }
