61f4627f34f1c511e046fa6b3e8f24cbb4cbcb17
Jonathan M. <neodev7440@gmail.com>
2026-05-03 16:04:34 +0200

    ajout du test de verfification de la méthode save() qui doit créer une niuvelle séance si l'id est null

--- src/test/java/fr/fms/Distopia/service/SeanceServiceTest.java
+++ src/test/java/fr/fms/Distopia/service/SeanceServiceTest.java
@@ -7,6 +7,8 @@ import fr.fms.Distopia.entities.Cinema;
 import fr.fms.Distopia.entities.Movie;
 import fr.fms.Distopia.entities.Seance;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -14,7 +16,12 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
  class SeanceServiceTest {
@@ -50,5 +57,22 @@ import java.util.ArrayList;
         seance.setMovie(movie);
         seance.setCinema(cinema);
         seance.setReservations(new ArrayList<>());
+    }
+
+    //------------------tests du save()-----------------------
+    @Test
+    @DisplayName("save() - creates a new seance when id is null")
+    void save_shouldCreateNewSeanceWhenIdIsNull() {
+        when(movieRepository.findById(1L)).thenReturn(Optional.of(movie));
+        when(cinemaRepository.findById(1L)).thenReturn(Optional.of(cinema));
+        when(seanceRepository.save(any(Seance.class))).thenAnswer(i -> i.getArgument(0));
+
+        Seance result = seanceService.save(null, futureDate, 100, 10.0,1L,1L);
+
+        assertThat(result.getAvailableSeats()).isEqualTo(100);
+        assertThat(result.getPrice()).isEqualTo(10.0);
+        assertThat(result.getCinema()).isEqualTo(cinema);
+        assertThat(result.getMovie()).isEqualTo(movie);
+        verify(seanceRepository).save(any(Seance.class));
     }
 }
