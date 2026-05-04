b984c6225701b58aaf630f67f1c233a64cb927f1
Jonathan M. <neodev7440@gmail.com>
2026-05-04 08:15:20 +0200

    ajout de la classe de test pour IndexController + setup de base et test de la view

---
+++ src/test/java/fr/fms/Distopia/web/IndexControllerTest.java
@@ -0,0 +1,54 @@
+package fr.fms.Distopia.web;
+
+import fr.fms.Distopia.entities.Town;
+import fr.fms.Distopia.service.TownService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.ui.Model;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class IndexControllerTest {
+    @Mock
+    private TownService townService;
+    @Mock
+    private Model model;
+    @InjectMocks
+    private IndexController indexController;
+
+    List<Town> towns;
+
+    @BeforeEach
+    void setUp() {
+        Town paris = new Town();
+        paris.setId(1L);
+        paris.setName("Paris");
+
+        Town lyon =  new Town();
+        lyon.setId(2L);
+        lyon.setName("Lyon");
+
+        towns = List.of(paris, lyon);
+    }
+
+    //------------------test for index()---------------------------------
+    @Test
+    @DisplayName("index() - returns view name 'index")
+    void index_ShouldReturnIndexView() {
+        when(townService.getAll()).thenReturn(towns);
+
+        String view = indexController.index(model);
+
+        assertThat(view).isEqualTo("index");
+    }
+
+}
