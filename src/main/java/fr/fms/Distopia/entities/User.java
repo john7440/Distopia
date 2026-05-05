05c4b06f28830800722e655a43b87f5ce273f7c1
Jonathan M. <neodev7440@gmail.com>
2026-05-05 15:37:20 +0200

    ajout de userDetails pour Spring Security + les méthodes nécessaire

--- src/main/java/fr/fms/Distopia/entities/User.java
+++ src/main/java/fr/fms/Distopia/entities/User.java
@@ -4,8 +4,13 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
 
 /**
  * Represents a User entity in the application
@@ -24,7 +29,7 @@ import java.io.Serializable;
 @Table(name="users")
 @Data
 @AllArgsConstructor @NoArgsConstructor
-public class User implements Serializable {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -35,4 +40,33 @@ public class User implements Serializable {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Role role = Role.USER;
+
+    //-------------------ajout partie UserDetails----------------------------
+    /**
+     * Retourne les autorités de l'utilisateur
+     * Spring Security attend le préfixe "ROLE_" : ROLE_ADMIN ou ROLE_USER
+     */
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities(){
+        return List.of(new SimpleGrantedAuthority("ROLE_"+ role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired(){
+        return true;
+    }
+    @Override
+    public boolean isAccountNonLocked(){
+        return true;
+    }
+    @Override
+    public boolean isCredentialsNonExpired(){
+        return true;
+    }
+    @Override
+    public boolean isEnabled(){
+        return true;
+    }
+
 }
+
