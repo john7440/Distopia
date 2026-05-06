03126b6b649450e73cb291c4f230ebe7af3b2070
Jonathan M. <neodev7440@gmail.com>
2026-05-06 08:57:37 +0200

    modifications suite a l'implémentation de Spring-Security, le httpSession n'est plus utilisé

--- src/main/java/fr/fms/Distopia/web/TownController.java
+++ src/main/java/fr/fms/Distopia/web/TownController.java
@@ -2,8 +2,6 @@ package fr.fms.Distopia.web;
 
 import fr.fms.Distopia.dao.TownRepository;
 import fr.fms.Distopia.service.TownService;
-import fr.fms.Distopia.utils.SessionUtils;
-import jakarta.servlet.http.HttpSession;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -25,8 +23,7 @@ public class TownController {
      * Displays the admin town management page
      */
     @GetMapping("/admin/towns")
-    public String towns(@RequestParam(required = false) Long editId, Model model, HttpSession session) {
-        if (SessionUtils.isNotAdmin(session)) return SessionUtils.REDIRECTION;
+    public String towns(@RequestParam(required = false) Long editId, Model model) {
         model.addAttribute("towns", townService.getAll());
         if (editId != null) {
             townRepository.findById(editId).ifPresent(town -> model.addAttribute("editTown", town));
@@ -41,13 +38,11 @@ public class TownController {
      *
      * @param id  the ID of the town to update; if null, a new town is created
      * @param name the name to assign to the town
-     * @param session the current HTTP session, used to verify admin access
      * @return a redirect to /admin/towns on success, or to
      *         /index if the user is not an admin
      */
     @PostMapping("/admin/saveTown")
-    public String saveTown(@RequestParam(required = false) Long id,@RequestParam String name, HttpSession session) {
-        if(SessionUtils.isNotAdmin(session)) return SessionUtils.REDIRECTION;
+    public String saveTown(@RequestParam(required = false) Long id,@RequestParam String name) {
         townService.save(id, name);
         return "redirect:/admin/towns";
     }
@@ -58,14 +53,12 @@ public class TownController {
      * Deletes a Town by its id
      *
      * @param id the ID of the town to delete
-     * @param session the current HTTP session, used to verify admin access
      * @return a redirect to /admin/towns on success, or to
      *         /index if the user is not an admin
      */
     @Transactional
     @GetMapping("/admin/deleteTown")
-    public String deleteTown(@RequestParam Long id, HttpSession session) {
-        if(SessionUtils.isNotAdmin(session)) return SessionUtils.REDIRECTION;
+    public String deleteTown(@RequestParam Long id) {
         townService.delete(id);
         return "redirect:/admin/towns";
     }
