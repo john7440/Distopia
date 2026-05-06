597766dac0a3e4c83d55e52d59d6e9a2661081c7
Jonathan M. <neodev7440@gmail.com>
2026-05-06 12:48:22 +0200

    ajout d'un dto pour les films avec attributs nécessaire + list de genre

---
+++ src/main/java/fr/fms/Distopia/tmdb/dto/TmdbMovieDto.java
@@ -0,0 +1,35 @@
+package fr.fms.Distopia.tmdb.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+/**
+ * DTO représentant un film retourné par l'API TMDB
+ * @JsonIgnoreProperties(ignoreUnknown = true) permet d'ignorer les attributs
+ * non nécessaires
+ */
+
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TmdbMovieDto {
+
+    private long id;
+    private String title;
+    private String overview;
+
+    // JSON snake_case -> Java camelCase
+    @JsonProperty("poster_path")
+    private String posterPath;
+
+    @JsonProperty("release_date")
+    private String releaseDate;
+
+    private Integer runtime;
+    private List<TmdbGenreDto> genres;
+
+}
