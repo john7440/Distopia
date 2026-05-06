9116f379ae1a6b2c8ff7df2a9bb2eadbc8a5d5d5
Jonathan M. <neodev7440@gmail.com>
2026-05-06 09:49:25 +0200

    mise a jour des test pour supprimer les mock de httpSession et utilisé une méthode authenticate() à la place + retrait des tests de redirection car géré par spring security

--- src/test/java/fr/fms/Distopia/web/UserControllerTest.java
+++ src/test/java/fr/fms/Distopia/web/UserControllerTest.java
@@ -3,7 +3,6 @@ package fr.fms.Distopia.web;
 import fr.fms.Distopia.entities.Role;
 import fr.fms.Distopia.entities.User;
 import fr.fms.Distopia.service.UserService;
-import jakarta.servlet.http.HttpSession;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -24,8 +23,6 @@ class UserControllerTest {
     private UserService userService;
     @Mock
     private Model model;
-    @Mock
-    private HttpSession session;
 
     @InjectMocks
     private UserController userController;
@@ -44,71 +41,15 @@ class UserControllerTest {
     @Test
     @DisplayName("loginPage() - returns 'login' view")
     void loginPage_ShouldReturnLoginView() {
-        String view = userController.loginPage(null, session,model);
+        String view = userController.loginPage(null,model);
 
         assertThat(view).isEqualTo("login");
     }
 
-    @Test
-    @DisplayName("loginPage() - stores redirect param in session when present")
-    void loginPage_ShouldStoresRedirectParamInSessionWhenPresent() {
-        userController.loginPage("/seances?movieId=1&cinemaId=2",session,model);
 
-        verify(session).setAttribute("redirectAfterLogin","/seances?movieId=1&cinemaId=2");
-    }
-
-    @Test
-    @DisplayName("loginPage() - does not touch session when redirect is null")
-    void loginPage_ShouldNotTouchSessionWhenRedirectIsNull() {
-        userController.loginPage(null,session,model);
-
-        verify(session, never()).setAttribute(eq("redirectAfterLogin"),any());
-    }
-
-    @Test
-    @DisplayName("loginPage() - adds redirect param to model")
-    void loginPage_ShouldAddRedirectPAramToModel() {
-        userController.loginPage("/my-reservations",session,model);
-
-        verify(session).setAttribute("redirectAfterLogin","/my-reservations");
-    }
 
     //------------------tests for login() (POST)------------------------------
-    @Test
-    @DisplayName("login() - redirects to /index on successful login with no pending redirect")
-    void loginPage_ShouldRedirectToIndexOnSuccessfulLoginAndNoRedirectStored() {
-        when(userService.login("John","pass123")).thenReturn(Optional.of(user));
-        when(session.getAttribute("redirectAfterLogin")).thenReturn(null);
 
-        String view = userController.login("John","pass123", session,model);
-
-        assertThat(view).isEqualTo("redirect:/index");
-        verify(session).setAttribute("connectedUser",user);
-        verify(session).removeAttribute("redirectAfterLogin");
-    }
-
-    @Test
-    @DisplayName("login() - redirects to stored redirect Url on successful login")
-    void loginPage_ShouldRedirectToStoredRedirectUrlOnSuccessfulLogin() {
-        when(userService.login("John","pass123")).thenReturn(Optional.of(user));
-        when(session.getAttribute("redirectAfterLogin")).thenReturn("/my-reservations");
-
-        String view = userController.login("John","pass123", session,model);
-
-        assertThat(view).isEqualTo("redirect:/my-reservations");
-    }
-
-    @Test
-    @DisplayName("login() - returns 'login' view with error on wrong credentials")
-    void loginPage_ShouldReturnLoginViewWithErrorOnWrongCredentials() {
-        when(userService.login("John","pass123")).thenReturn(Optional.empty());
-
-        String view = userController.login("John","pass123",session, model);
-
-        assertThat(view).isEqualTo("login");
-        verify(model).addAttribute("error","Identifiants incorrects");
-        verify(session,never()).setAttribute(eq("connectedUser"),any());
-    }
 
     //-----------------test for registerPage()-----------------
     @Test
@@ -119,14 +60,13 @@ class UserControllerTest {
 
     //------------------------tests for register() (POST) --------------------------
     @Test
-    @DisplayName("register() - stores user in session and redirects to /index on success")
+    @DisplayName("register() - stores user in session and redirects to /login registered on success")
     void registerPage_ShouldStoresUserInSessionAndRedirectToIndexOnSuccess() {
         when(userService.register("John","pass123")).thenReturn(Optional.of(user));
 
-        String view = userController.register("John","pass123", session,model);
+        String view = userController.register("John","pass123",model);
 
-        assertThat(view).isEqualTo("redirect:/index");
-        verify(session).setAttribute("connectedUser",user);
+        assertThat(view).isEqualTo("redirect:/login?registered=true");
     }
 
     @Test
@@ -134,21 +74,10 @@ class UserControllerTest {
     void registerPage_ShouldReturnRegisterViewWithErrorWhenUsernameIsTaken() {
         when(userService.register("John","pass123")).thenReturn(Optional.empty());
 
-        String view = userController.register("John","pass123", session,model);
+        String view = userController.register("John","pass123",model);
 
         assertThat(view).isEqualTo("register");
         verify(model).addAttribute("error","Ce nom d'utilisateur est déjà pris");
-        verify(session, never()).setAttribute(eq("connectedUser"),any());
     }
 
-    //-------------------------------------test for logout()------------------
-
-    @Test
-    @DisplayName("logout() - invalidates session and redirect to /index")
-    void logout_ShouldInvalidatesSessionAndRedirectToIndexOnSuccess() {
-        String view = userController.logout(session);
-
-        assertThat(view).isEqualTo("redirect:/index");
-        verify(session).invalidate();
-    }
 }
