36bc6adaf4c1dd22f4970881700867063b037969
Jonathan M. <neodev7440@gmail.com>
2026-05-07 09:19:13 +0200

    ajout d'une classe de test pour tester le comportement de Security Config

---
+++ src/test/java/fr/fms/Distopia/config/SecurityConfigTest.java
@@ -0,0 +1,13 @@
+package fr.fms.Distopia.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class SecurityConfigTest {
+    @Autowired
+    private MockMvc mockMvc;
+}
