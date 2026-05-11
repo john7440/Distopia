d6ce936d8ed80d062acd6d0e76e4bff912cf0c0f
Jonathan M. <neodev7440@gmail.com>
2026-05-11 07:35:35 +0200

    ajout d'un query avec pagination pour les seances

--- src/main/java/fr/fms/Distopia/dao/SeanceRepository.java
+++ src/main/java/fr/fms/Distopia/dao/SeanceRepository.java
@@ -3,6 +3,8 @@ package fr.fms.Distopia.dao;
 
 import fr.fms.Distopia.entities.Seance;
 import jakarta.persistence.LockModeType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
@@ -25,4 +27,13 @@ public interface SeanceRepository extends JpaRepository<Seance, Long> {
     Optional<Seance> findByIdForUpdate(@Param("id") Long id);
     List<Seance> findByMovieIdAndCinemaIdOrderByDateTimeAsc(Long movieId, Long cinemaId);
     List<Seance> findByMovieIdAndDateTimeAfterOrderByDateTimeAsc(Long movieId, LocalDateTime after);
+
+    @Query("SELECT s FROM Seance s " +
+            "JOIN s.movie m JOIN s.cinema c WHERE " +
+            "(:keyword IS NULL OR :keyword = '' OR " +
+            " LOWER(m.title) LIKE LOWER(CONCAT('%', :keyword, '%'))) AND " +
+            "(:cinemaId IS NULL OR c.id = :cinemaId) " +
+            "ORDER BY s.dateTime ASC")
+    Page<Seance> searchAdmin(@Param("keyword") String keyword, @Param("cinemaId") Long cinemaId, Pageable pageable);
+
 }
