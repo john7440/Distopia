47271e387485cab5f4a1c8f0a940362509375aac
Jonathan M. <neodev7440@gmail.com>
2026-05-11 07:46:32 +0200

    ajout d'un query avec pagination pour les films

--- src/main/java/fr/fms/Distopia/dao/MovieRepository.java
+++ src/main/java/fr/fms/Distopia/dao/MovieRepository.java
@@ -1,7 +1,11 @@
 package fr.fms.Distopia.dao;
 
 import fr.fms.Distopia.entities.Movie;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,4 +17,10 @@ import java.util.List;
 public interface MovieRepository extends JpaRepository<Movie,Long> {
     List<Movie> findByCinemasIdAndDeletedFalse(Long cinemaId);
     List<Movie> findByDeletedFalseOrderByTitleAsc();
+
+    @Query("SELECT m FROM Movie m WHERE " +
+            "(:keyword IS NULL OR :keyword = '' OR " +
+            " LOWER(m.title) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            " LOWER(m.genre) LIKE LOWER(CONCAT('%', :keyword, '%')))")
+    Page<Movie> searchAdmin(@Param("keyword") String keyword, Pageable pageable);
 }
