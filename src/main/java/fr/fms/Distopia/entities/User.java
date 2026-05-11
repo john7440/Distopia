0ad1a25549c0bbafd32218dab17c5dce3c24c64d
Jonathan M. <neodev7440@gmail.com>
2026-05-11 15:13:52 +0200

    ajout d'un attribut email pour les utilisateurs

--- src/main/java/fr/fms/Distopia/entities/User.java
+++ src/main/java/fr/fms/Distopia/entities/User.java
@@ -37,6 +37,8 @@ public class User implements UserDetails {
     private String username;
     @Column(nullable = false)
     private String password;
+    @Column(unique = true)
+    private String email;
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Role role = Role.USER;
