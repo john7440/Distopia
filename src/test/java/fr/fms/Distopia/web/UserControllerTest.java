ce8bcc2ccf5ba87fca01e15817278ff09f29dd74
Jonathan M. <neodev7440@gmail.com>
2026-05-12 08:46:33 +0200

    modification des test pour ajouter l'attribut email qui est maintenant demandé

--- src/test/java/fr/fms/Distopia/web/UserControllerTest.java
+++ src/test/java/fr/fms/Distopia/web/UserControllerTest.java
@@ -37,47 +37,26 @@ class UserControllerTest {
         user.setRole(Role.USER);
     }
 
-    //--------------------------tests for loginPage() ---------------------
-    @Test
-    @DisplayName("loginPage() - returns 'login' view")
-    void loginPage_ShouldReturnLoginView() {
-        String view = userController.loginPage(null,model);
-
-        assertThat(view).isEqualTo("login");
-    }
-
-
-
-    //------------------tests for login() (POST)------------------------------
-
-
-    //-----------------test for registerPage()-----------------
-    @Test
-    @DisplayName("registerPage() - return 'register' view")
-    void registerPage_ShouldReturnRegisterView() {
-        assertThat(userController.registerPage()).isEqualTo("register");
-    }
 
     //------------------------tests for register() (POST) --------------------------
     @Test
     @DisplayName("register() - stores user in session and redirects to /login registered on success")
     void registerPage_ShouldStoresUserInSessionAndRedirectToIndexOnSuccess() {
-        when(userService.register("John","pass123")).thenReturn(Optional.of(user));
+        when(userService.register("John","monemail@test.com","pass123")).thenReturn(Optional.of(user));
 
-        String view = userController.register("John","pass123",model);
+        String view = userController.register("John","monemail@test.com","pass123");
 
-        assertThat(view).isEqualTo("redirect:/login?registered=true");
+        assertThat(view).isEqualTo("redirect:/?registered");
     }
 
     @Test
     @DisplayName("register() - returns 'register' view with error when username is taken")
     void registerPage_ShouldReturnRegisterViewWithErrorWhenUsernameIsTaken() {
-        when(userService.register("John","pass123")).thenReturn(Optional.empty());
+        when(userService.register("John","monemail@test.com","pass123")).thenReturn(Optional.empty());
 
-        String view = userController.register("John","pass123",model);
+        String view = userController.register("John","monemail@test.com","pass123");
 
-        assertThat(view).isEqualTo("register");
-        verify(model).addAttribute("error","Ce nom d'utilisateur est déjà pris");
+        assertThat(view).isEqualTo("redirect:/?openRegister&registerError");
     }
 
 }
