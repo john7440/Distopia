974341729f82e32b569bf8ad1ef1e15cdc8ccbca
Jonathan M. <neodev7440@gmail.com>
2026-05-21 07:20:08 +0200

    ajout d'une classe de test pour SeanceGeneratorService + setup de base

---
+++ src/test/java/fr/fms/Distopia/service/SeanceGeneratorServiceTest.java
@@ -0,0 +1,64 @@
+package fr.fms.Distopia.service;
+
+import fr.fms.Distopia.entities.Cinema;
+import fr.fms.Distopia.entities.Movie;
+import fr.fms.Distopia.tmdb.TmdbClient;
+import fr.fms.Distopia.tmdb.dto.TmdbGenreDto;
+import fr.fms.Distopia.tmdb.dto.TmdbMovieDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+@ExtendWith(MockitoExtension.class)
+class SeanceGeneratorServiceTest {
+    @Mock
+    private TmdbClient tmdbClient;
+    @Mock
+    private MovieService movieService;
+    @Mock
+    SeanceService seanceService;
+    @Mock
+    private CinemaService cinemaService;
+    @InjectMocks
+    private SeanceGeneratorService seanceGeneratorService;
+
+    private Cinema cinema;
+    private Movie movie;
+    private TmdbMovieDto tmdbMovieDto;
+    private TmdbMovieDto tmdbDetail;
+
+    private static final LocalDateTime START_DATE = LocalDateTime.now().plusDays(1).withHour(0)
+            .withMinute(0).withSecond(0).withNano(0);
+
+    @BeforeEach
+    void setUp() {
+        Cinema cinema = new Cinema();
+        cinema.setId(1L);
+        cinema.setName("Cinema Test");
+
+        movie = new Movie();
+        movie.setId(1L);
+        movie.setTitle("Inception");
+
+        TmdbGenreDto genre =  new TmdbGenreDto();
+        genre.setName("Sci-Fi");
+
+        tmdbMovieDto = new TmdbMovieDto();
+        tmdbMovieDto.setId(1L);
+        tmdbMovieDto.setTitle("Inception");
+
+        tmdbDetail = new TmdbMovieDto();
+        tmdbDetail.setId(1L);
+        tmdbDetail.setTitle("Inception");
+        tmdbDetail.setOverview("Description Inception");
+        tmdbDetail.setRuntime(148);
+        tmdbDetail.setPosterPath("/inception.jpg");
+        tmdbDetail.setGenres(List.of(genre));
+    }
+}
