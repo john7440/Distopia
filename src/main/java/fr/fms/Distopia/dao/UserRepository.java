be6afa76d25be4641d4b9e3f986b75ae63c2a72d
Jonathan M. <neodev7440@gmail.com>
2026-05-22 15:49:29 +0200

    ajout de la javadoc complète

--- src/main/java/fr/fms/Distopia/dao/UserRepository.java
+++ src/main/java/fr/fms/Distopia/dao/UserRepository.java
@@ -7,9 +7,17 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 /**
- * The JPA Repository of users
+ * JPA repository used to manage {@link User} entities
+ * <p>
+ * Provides CRUD operations and authentication-related queries
  */
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
+    /**
+     * Searches a user using their username
+     *
+     * @param username the username used for authentication
+     * @return an optional containing the matching user if found
+     */
     Optional<User> findByUsername(String username);
 }
