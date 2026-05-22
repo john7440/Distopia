21ae20212719ba35cea15a3bc5a41f39aacc03c4
Jonathan M. <neodev7440@gmail.com>
2026-05-22 10:55:39 +0200

    ajout de test pour builMapsUrl() - verification de retour d'adresse encodé si les coordonnées n'existent pas

--- src/test/java/fr/fms/Distopia/entities/CinemaTest.java
+++ src/test/java/fr/fms/Distopia/entities/CinemaTest.java
@@ -26,4 +26,18 @@ class CinemaTest {
         assertThat(result).isEqualTo("https://www.google.com/maps?q=57.7102,-1.2345");
     }
 
+    @Test
+    @DisplayName("buildMapsUrl() - returns encoded address url when coordinates do not exist")
+    void buildMapsUrl_ShouldReturnEncodedAddressUrlWhenCoordinatesDoNotExist() {
+        Town town = new Town();
+        town.setName("Dax");
+
+        cinema.setAddress("10 rue de la paix");
+        cinema.setTown(town);
+
+        String result = cinema.buildMapsUrl();
+
+        assertThat(result).contains("https://www.google.com/maps/search/?api=1&query=");
+        assertThat(result).contains("10+rue+de+la+paix+Dax");
+    }
 }
