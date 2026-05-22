9f8c1532f220ac16958a322ab7692545615584f5
Jonathan M. <neodev7440@gmail.com>
2026-05-22 14:17:20 +0200

    ajout de la javadoc complète

--- src/main/java/fr/fms/Distopia/dao/MovieRepository.java
+++ src/main/java/fr/fms/Distopia/dao/MovieRepository.java
@@ -14,15 +14,57 @@ import java.util.List;
 import java.util.Optional;
 
 /**
- * The JPA Repository of movies
+ * JPA repository used to manage {@link Movie} entities
+ * <p>
+ * Provides CRUD operations as well as custom queries
+ * for movie filtering, administration and TMDB integration
  */
 @Repository
 public interface MovieRepository extends JpaRepository<Movie,Long> {
+
+    /**
+     * Retrieves non-deleted movies associated with a cinema
+     *
+     * @param cinemaId the cinema identifier
+     * @param sort the sorting configuration
+     * @return the list of visible movies for the given cinema
+     */
     List<Movie> findByCinemasIdAndDeletedFalse(Long cinemaId, Sort sort);
+
+    /**
+     * Retrieves all non-deleted movies
+     *
+     * @param sort the sorting configuration
+     * @return the list of visible movies
+     */
     List<Movie> findByDeletedFalse(Sort sort);
-    Optional<Movie> findByTitleIgnoreCase(String title);
+
+    /**
+     * Searches a movie using its TMDB identifier
+     *
+     * @param tmdbId the TMDB movie identifier
+     * @return an optional containing the matching movie if found
+     */
     Optional<Movie> findByTmdbId(Long tmdbId);
 
+    /**
+     * Searches movies for the administration dashboard
+     * <p>
+     * Supports:
+     * <ul>
+     *     <li>keyword filtering on movie titles</li>
+     *     <li>optional inclusion of deleted movies</li>
+     *     <li>pagination</li>
+     * </ul>
+     * <p>
+     * Cinemas are eagerly loaded using {@link EntityGraph}
+     * to reduce lazy loading queries
+     *
+     * @param keyword the keyword used to search movie titles
+     * @param showDeleted whether deleted movies should be included
+     * @param pageable the pagination configuration
+     * @return a paginated list of matching movies
+     */
     @EntityGraph(attributePaths = {"cinemas"})
     @Query(value = """
         SELECT DISTINCT m FROM Movie m
