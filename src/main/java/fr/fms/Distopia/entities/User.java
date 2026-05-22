bf484e20934b0ff29eb94877a983eaa2a58140ee
Jonathan M. <neodev7440@gmail.com>
2026-05-22 11:33:33 +0200

    retrait de méthodes non utilisées

--- src/main/java/fr/fms/Distopia/entities/User.java
+++ src/main/java/fr/fms/Distopia/entities/User.java
@@ -52,23 +52,5 @@ public class User implements UserDetails {
     public Collection<? extends GrantedAuthority> getAuthorities(){
         return List.of(new SimpleGrantedAuthority("ROLE_"+ role.name()));
     }
-
-    @Override
-    public boolean isAccountNonExpired(){
-        return true;
-    }
-    @Override
-    public boolean isAccountNonLocked(){
-        return true;
-    }
-    @Override
-    public boolean isCredentialsNonExpired(){
-        return true;
-    }
-    @Override
-    public boolean isEnabled(){
-        return true;
-    }
-
 }
 
