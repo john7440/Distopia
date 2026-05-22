a6828dc507c08ba0e931fffcafa891db32321c21
Jonathan M. <neodev7440@gmail.com>
2026-05-22 15:47:01 +0200

    ajout de la javadoc complète

--- src/main/java/fr/fms/Distopia/dao/TownRepository.java
+++ src/main/java/fr/fms/Distopia/dao/TownRepository.java
@@ -8,9 +8,16 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 /**
- * The JPA Repository of towns
+ * JPA repository used to manage {@link Town} entities
+ * <p>
+ * Provides CRUD operations and lookup queries for towns
  */
 @Repository
 public interface TownRepository extends JpaRepository<Town,Long> {
+    /**
+     * Searches a town using its name
+     * @param name the town name
+     * @return an optional containing the matching town if found
+     */
     Optional<Town> findByName(String name);
 }
