23f12c75fa78074689636737f47784aaec2e5849
Jonathan M. <neodev7440@gmail.com>
2026-05-25 15:10:45 +0200

    ajout de la javadoc

--- src/main/java/fr/fms/Distopia/tmdb/dto/TmdbSearchResponse.java
+++ src/main/java/fr/fms/Distopia/tmdb/dto/TmdbSearchResponse.java
@@ -6,11 +6,16 @@ import lombok.Setter;
 
 import java.util.List;
 
+/**
+ * Data Transfer Object representing a TMDB search response
+ * <p>
+ * Used to deserialize paginated movie search results
+ * returned by the TMDB API
+ */
 @Setter
 @Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class TmdbSearchResponse {
 
     private List<TmdbMovieDto> results;
-
 }
