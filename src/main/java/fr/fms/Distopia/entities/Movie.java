5f05c81e2cc1a43bb0d500203cb6ea031e4d56f7
Jonathan M. <neodev7440@gmail.com>
2026-05-26 11:19:22 +0200

    ajout des contraintes de validation des attributs

--- src/main/java/fr/fms/Distopia/entities/Movie.java
+++ src/main/java/fr/fms/Distopia/entities/Movie.java
@@ -1,6 +1,7 @@
 package fr.fms.Distopia.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -28,18 +29,34 @@ public class Movie implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Positive(message = "L'identifiant TMDB doit être positif")
     @Column(unique = true)
     private Long tmdbId;
+
+    @NotBlank(message = "Le titre est obligatoire")
+    @Size(min = 1, max = 150, message = "Le titre ne doit pas dépasser 150 caractères")
     @Column(nullable = false)
     private String title;
+
+    @Size(max = 5000, message = "La description ne doit pas dépasser 5000 caractères")
     @Column(columnDefinition = "TEXT")
     private String description;
+
+    @Min(value = 0, message = "La durée ne peut pas être négative")
+    @Max(value = 600, message = "La durée ne peut pas dépasser 600 minutes")
     private int duration;
+
+    @Size(max = 80, message = "Le genre ne doit pas dépasser 80 caractères")
     private String genre;
+
     @Column(nullable = false)
     private boolean deleted = false;
+
     private String imageUrl;
+
     private String trailerUrl;
+
     @Column(name = "release_date")
     private LocalDate releaseDate;
 
