a66d2c27844413d84b839812024cc773c3bf5aa3
Jonathan M. <neodev7440@gmail.com>
2026-05-26 10:24:17 +0200

    remplacement des @Autowired par injection contructeur

--- src/main/java/fr/fms/Distopia/service/UserService.java
+++ src/main/java/fr/fms/Distopia/service/UserService.java
@@ -3,7 +3,6 @@ package fr.fms.Distopia.service;
 import fr.fms.Distopia.dao.UserRepository;
 import fr.fms.Distopia.entities.Role;
 import fr.fms.Distopia.entities.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,10 +12,13 @@ import java.util.Optional;
 @Service
 public class UserService {
 
-    @Autowired
-    private UserRepository userRepository;
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     //-----------------le login------------------------------------
     /**
