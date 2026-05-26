1df32c39e183315c93b17e0349edba24acbb08fe
Jonathan M. <neodev7440@gmail.com>
2026-05-26 07:59:38 +0200

    ajout de la javadoc

--- src/main/java/fr/fms/Distopia/DistopiaApplication.java
+++ src/main/java/fr/fms/Distopia/DistopiaApplication.java
@@ -3,9 +3,19 @@ package fr.fms.Distopia;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main entry point of the Distopia Spring Boot application
+ * <p>
+ * This class bootstraps and launches the application
+ * using Spring Boot
+ */
 @SpringBootApplication
 public class DistopiaApplication {
 
+    /**
+     * Starts the Distopia application
+     * @param args application startup arguments
+     */
 	public static void main(String[] args) {
 		SpringApplication.run(DistopiaApplication.class, args);
 	}
@@ -13,6 +23,4 @@ public class DistopiaApplication {
 // - admin : admin123
 // - alice: password
 // - jonatemps : password
-// - test123 : pass123
-// - test3456 : test3456
 }
