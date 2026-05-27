7ebf0ee438b090e96716e6b9df67c9319e02c8ba
Jonathan M. <neodev7440@gmail.com>
2026-05-27 08:46:15 +0200

    retrait import non utilisé

--- src/main/java/fr/fms/Distopia/web/SeanceController.java
+++ src/main/java/fr/fms/Distopia/web/SeanceController.java
@@ -16,8 +16,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.time.LocalDateTime;
-
 /**
  * Controller responsible for handling seance related web requests
  * <p>
