0cc9ac9c07597741311822f9902f2d2563e1dcde
Jonathan M. <neodev7440@gmail.com>
2026-06-03 10:33:21 +0200

    ajout test getFormattedRealeaseDate + utilisation de @ParameterizedTest pour éviter la répétition

--- src/test/java/fr/fms/Distopia/tmdb/dto/TmdbMovieDtoTest.java
+++ src/test/java/fr/fms/Distopia/tmdb/dto/TmdbMovieDtoTest.java
@@ -2,6 +2,9 @@ package fr.fms.Distopia.tmdb.dto;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,24 +22,41 @@ class TmdbMovieDtoTest {
         assertThat(result).isEqualTo("2026");
     }
 
-    @Test
-    @DisplayName("getReleaseYear() - should return dash when release date is null")
-    void getReleaseYear_ShouldReturnDashWhenReleaseDateIsNull() {
+    @ParameterizedTest
+    @NullSource
+    @ValueSource(strings = {"20"})
+    @DisplayName("getReleaseYear() - should return dash when release date is null or too short")
+    void getReleaseYear_ShouldReturnDashWhenReleaseDateIsNull(String ignoredInput) {
         TmdbMovieDto movie = new TmdbMovieDto();
+        movie.setReleaseDate(null);
 
         String result = movie.getReleaseYear();
 
         assertThat(result).isEqualTo("—");
     }
 
+
+    //--------------------tests for getFormattedReleaseDate() ------------------
     @Test
-    @DisplayName("getReleaseYear() - should return dash when release date is too short")
-    void getReleaseYear_ShouldReturnDashWhenReleaseDateIsTooShort() {
+    @DisplayName("getFormattedReleaseDate() - should return formatted release date")
+    void getFormattedReleaseDate_ShouldReturnFormattedReleaseDate() {
         TmdbMovieDto movie = new TmdbMovieDto();
-        movie.setReleaseDate("20");
+        movie.setReleaseDate("2026-05-26");
 
-        String result = movie.getReleaseYear();
+        String result = movie.getFormattedReleaseDate();
 
-        assertThat(result).isEqualTo("—");
+        assertThat(result).isEqualTo("Sortie le 26 mai 2026");
+    }
+
+    @ParameterizedTest
+    @NullSource
+    @ValueSource(strings = {" ", "invalide-date"})
+    @DisplayName("getFormattedReleaseDate() - should return A venir when release date is null, blank or invalid")
+    void getFormattedReleaseDate_ShouldReturnAVenirWhenReleaseDateIsNull(String ignoredInput) {
+        TmdbMovieDto movie = new TmdbMovieDto();
+
+        String result = movie.getFormattedReleaseDate();
+
+        assertThat(result).isEqualTo("A venir");
     }
 }
